a5ad063852da5df1f0ae650eb949b2f470965cf0
Bob Mottram <bob@freedombone.net>
2019-05-13 13:58:54 +0100

    Add universe

--- installer.sh
+++ installer.sh
@@ -1,6 +1,13 @@
-sudo apt-get -y install python3
-sudo apt-get -y install python3-pip
+#!/bin/bash
+INSTALL_PACKAGE="sudo apt-get -y install"
+PYTHON_PACKAGE="python3"
+if [ -f /usr/bin/pacman ]; then
+    INSTALL_PACKAGE="sudo pacman -S --noconfirm"
+    PYTHON_PACKAGE="python"
+fi
+$INSTALL_PACKAGE $PYTHON_PACKAGE
+$INSTALL_PACKAGE $PYTHON_PACKAGE-pip
 yes | sudo pip3 install commentjson
 yes | sudo pip3 install websocket-client
-sudo apt-get -y install git-core
-git clone https://github.com/wowpin/dumserver.git
+$INSTALL_PACKAGE git-core
+git clone https://code.freedombone.net/bashrc/AberMUSH
