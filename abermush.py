e10ce21296682c7deee2e9c476d9a5e3ce22e1c8
Bob Mottram <bob@freedombone.net>
2019-05-24 10:16:43 +0100

    Separate module for combat

--- abermush.py
+++ abermush.py
@@ -30,6 +30,7 @@ from events import evaluateEvent
 
 from commands import runCommand
 from atcommands import runAtCommand
+from combat import runFights
 
 import time
 
@@ -340,90 +341,7 @@ while True:
                                 players[pid]['hp'] = 4
 
         # Handle Fights
-        attack_types_pre=["strike","lunge","bludgeon","thrust","swipe","swing","stab","cut","slash"]
-        attack_types_pre2=["struck","lunged","bludgeoned","thrusted","swiped","swung","stabbed","cut","slashed"]
-        attack_types_post=["viciously at","savagely at","daringly at","a glancing blow on","a blow on","heavily at","clumsily at","crudely at"]
-        for (fid, pl) in list(fights.items()):
-                # PC -> PC
-                if fights[fid]['s1type'] == 'pc' and fights[fid]['s2type'] == 'pc':
-                        if players[fights[fid]['s1id']]['room'] == players[fights[fid]['s2id']]['room']:
-                                if int(time.time()) >= players[fights[fid]['s1id']]['lastCombatAction'] + 10 - players[fights[fid]['s1id']]['agi']:
-                                        if players[fights[fid]['s2id']]['isAttackable'] == 1:
-                                                players[fights[fid]['s1id']]['isInCombat'] = 1
-                                                players[fights[fid]['s2id']]['isInCombat'] = 1
-                                                # Do damage to the PC here
-                                                if randint(0, 1) == 1:
-                                                        modifier = randint(0, 10)
-                                                        if players[fights[fid]['s1id']]['hp'] > 0:
-                                                                players[fights[fid]['s2id']]['hp'] = players[fights[fid]['s2id']]['hp'] - (players[fights[fid]['s1id']]['str'] + modifier)
-                                                                players[fights[fid]['s1id']]['lastCombatAction'] = int(time.time())
-                                                                attackDescriptionIndex1=randint(0,len(attack_types_pre)-1)
-                                                                attackDescriptionIndex2=randint(0,len(attack_types_post)-1)
-                                                                attackDescription=attack_types_pre[attackDescriptionIndex1] + ' ' + attack_types_post[attackDescriptionIndex2]
-                                                                mud.send_message(fights[fid]['s1id'], 'You ' + attackDescription + ' <f32><u>' + players[fights[fid]['s2id']]['name'] + '<r> for <f15><b2> * ' + str(players[fights[fid]['s1id']]['str'] + modifier) + ' *<r> points of damage.\n')
-                                                                attackDescription=attack_types_pre2[attackDescriptionIndex1] + ' ' + attack_types_post[attackDescriptionIndex2]
-                                                                mud.send_message(fights[fid]['s2id'], '<f32>' + players[fights[fid]['s1id']]['name'] + '<r> has ' + attackDescription + ' you for <f15><b88> * ' + str(players[fights[fid]['s1id']]['str'] + modifier) + ' *<r> points of damage.\n')
-                                                else:
-                                                        players[fights[fid]['s1id']]['lastCombatAction'] = int(time.time())
-                                                        mud.send_message(fights[fid]['s1id'], 'You miss trying to hit <f32><u>' + players[fights[fid]['s2id']]['name'] + '\n')
-                                                        mud.send_message(fights[fid]['s2id'], '<f32><u>' + players[fights[fid]['s1id']]['name'] + '<r> missed while trying to hit you!\n')
-                                        else:
-                                                mud.send_message(fights[fid]['s1id'], '<f225>Suddenly you stop. It wouldn`t be a good idea to attack <f32>' + players[fights[fid]['s2id']]['name'] + ' at this time.\n')
-                                                fightsCopy = deepcopy(fights)
-                                                for (fight, pl) in fightsCopy.items():
-                                                        if fightsCopy[fight]['s1id'] == fights[fid]['s1id'] and fightsCopy[fight]['s2id'] == fights[fid]['s2id']:
-                                                                del fights[fight]
-                # PC -> NPC
-                elif fights[fid]['s1type'] == 'pc' and fights[fid]['s2type'] == 'npc':
-                        if players[fights[fid]['s1id']]['room'] == npcs[fights[fid]['s2id']]['room']:
-                                if int(time.time()) >= players[fights[fid]['s1id']]['lastCombatAction'] + 10 - players[fights[fid]['s1id']]['agi']:
-                                        if npcs[fights[fid]['s2id']]['isAttackable'] == 1:
-                                                players[fights[fid]['s1id']]['isInCombat'] = 1
-                                                npcs[fights[fid]['s2id']]['isInCombat'] = 1
-                                                # Do damage to the NPC here
-                                                if randint(0, 1) == 1:
-                                                        modifier = randint(0, 10)
-                                                        if players[fights[fid]['s1id']]['hp'] > 0:
-                                                                npcs[fights[fid]['s2id']]['hp'] = npcs[fights[fid]['s2id']]['hp'] - (players[fights[fid]['s1id']]['str'] + modifier)
-                                                                players[fights[fid]['s1id']]['lastCombatAction'] = int(time.time())
-
-                                                                attackDescriptionIndex1=randint(0,len(attack_types_pre)-1)
-                                                                attackDescriptionIndex2=randint(0,len(attack_types_post)-1)
-                                                                attackDescription=attack_types_pre[attackDescriptionIndex1] + ' ' + attack_types_post[attackDescriptionIndex2]
-
-                                                                mud.send_message(fights[fid]['s1id'], 'You '+ attackDescription + ' <f220>' + npcs[fights[fid]['s2id']]['name'] + '<r> for <b2><f15> * ' + str(players[fights[fid]['s1id']]['str'] + modifier)  + ' * <r> points of damage\n')
-
-                                                else:
-                                                        players[fights[fid]['s1id']]['lastCombatAction'] = int(time.time())
-                                                        mud.send_message(fights[fid]['s1id'], 'You miss <f220>' + npcs[fights[fid]['s2id']]['name'] + '<r> completely!\n')
-                                        else:
-                                                mud.send_message(fights[fid]['s1id'], '<f225>Suddenly you stop. It wouldn`t be a good idea to attack <u><f21>' + npcs[fights[fid]['s2id']]['name'] + '<r> at this time.\n')
-                                                fightsCopy = deepcopy(fights)
-                                                for (fight, pl) in fightsCopy.items():
-                                                        if fightsCopy[fight]['s1id'] == fights[fid]['s1id'] and fightsCopy[fight]['s2id'] == fights[fid]['s2id']:
-                                                                del fights[fight]
-                # NPC -> PC
-                elif fights[fid]['s1type'] == 'npc' and fights[fid]['s2type'] == 'pc':
-                        if npcs[fights[fid]['s1id']]['room'] == players[fights[fid]['s2id']]['room']:
-                                if int(time.time()) >= npcs[fights[fid]['s1id']]['lastCombatAction'] + 10 - npcs[fights[fid]['s1id']]['agi']:
-                                        npcs[fights[fid]['s1id']]['isInCombat'] = 1
-                                        players[fights[fid]['s2id']]['isInCombat'] = 1
-                                        # Do the damage to PC here
-                                        if randint(0, 1) == 1:
-                                                modifier = randint(0, 10)
-                                                if npcs[fights[fid]['s1id']]['hp'] > 0:
-                                                        players[fights[fid]['s2id']]['hp'] = players[fights[fid]['s2id']]['hp'] - (npcs[fights[fid]['s1id']]['str'] + modifier)
-                                                        npcs[fights[fid]['s1id']]['lastCombatAction'] = int(time.time())
-                                                        attackDescriptionIndex1=randint(0,len(attack_types_pre)-1)
-                                                        attackDescriptionIndex2=randint(0,len(attack_types_post)-1)
-                                                        attackDescription=attack_types_pre2[attackDescriptionIndex1] + ' ' + attack_types_post[attackDescriptionIndex2]
-                                                        mud.send_message(fights[fid]['s2id'], '<f220>' + npcs[fights[fid]['s1id']]['name'] + '<r> has ' + attackDescription + ' you for <f15><b88> * ' + str(npcs[fights[fid]['s1id']]['str'] + modifier) + ' * <r> points of damage.\n')
-                                        else:
-                                                npcs[fights[fid]['s1id']]['lastCombatAction'] = int(time.time())
-                                                mud.send_message(fights[fid]['s2id'], '<f220>' + npcs[fights[fid]['s1id']]['name'] + '<r> has missed you completely!\n')
-                elif fights[fid]['s1type'] == 'npc' and fights[fid]['s2type'] == 'npc':
-                        test = 1
-                        # NPC -> NPC
+        runFights(mud,players,npcs,fights)
 
         # Iterate through NPCs, check if its time to talk, then check if anyone is attacking it
         for (nid, pl) in list(npcs.items()):
