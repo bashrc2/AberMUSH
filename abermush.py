352334b60e46ea3f8659284e0cd7e16b44c90a07
Bob Mottram <bob@freedombone.net>
2019-05-24 12:42:37 +0100

    Separate out death functions

--- abermush.py
+++ abermush.py
@@ -35,6 +35,8 @@ from playerconnections import runPlayerConnections
 from playerconnections import disconnectIdlePlayers
 from npcs import npcRespawns
 from npcs import runNPCs
+from reaper import removeCorpses
+from reaper import runDeaths
 
 import time
 
@@ -314,33 +316,7 @@ while True:
                                 lastStateSave = now
 
         # Handle Player Deaths
-        for (pid, pl) in list(players.items()):
-                if players[pid]['authenticated'] == True:
-                        if players[pid]['hp'] <= 0:
-                                # Create player's corpse in the room
-                                corpses[len(corpses)] = { 'room': players[pid]['room'], 'name': str(players[pid]['name'] + '`s corpse'), 'inv': players[pid]['inv'], 'died': int(time.time()), 'TTL': players[pid]['corpseTTL'], 'owner': 1 }
-                                # Clear player's inventory, it stays on the corpse
-                                # This is bugged, causing errors when picking up things after death
-                                # players[pid]['inv'] = ''
-                                players[pid]['isInCombat'] = 0
-                                players[pid]['lastRoom'] = players[pid]['room']
-                                players[pid]['room'] = '$rid=666$'
-                                fightsCopy = deepcopy(fights)
-                                for (fight, pl) in fightsCopy.items():
-                                        if fightsCopy[fight]['s1id'] == pid or fightsCopy[fight]['s2id'] == pid:
-                                                del fights[fight]
-                                for (pid2, pl) in list(players.items()):
-                                        if players[pid2]['authenticated'] is not None \
-                                           and players[pid2]['room'] == players[pid]['lastRoom'] \
-                                           and players[pid2]['name'] != players[pid]['name']:
-                                                mud.send_message(pid2, '<u><f32>{}<r> <f124>has been killed.'.format(players[pid]['name']) + "\n")
-                                                players[pid]['lastRoom'] = None
-                                                mud.send_message(pid, '<b88><f158>Oh dear! You have died!\n')
-
-                                # Add Player Death event (ID:666) to eventSchedule
-                                addToScheduler(666, pid, eventSchedule, scriptedEventsDB)
-
-                                players[pid]['hp'] = 4
+        runDeaths(mud,players,corpses,fights)
 
         # Handle Fights
         runFights(mud,players,npcs,fights)
@@ -373,12 +349,7 @@ while True:
                                                 env[eid]['timeTalked'] = now
                                                 env[eid]['randomizer'] = randint(0, env[eid]['randomFactor'])
 
-        # Iterate through corpses and remove ones older than their TTL
-        corpsesCopy = deepcopy(corpses)
-        for (c, pl) in corpsesCopy.items():
-                if int(time.time()) >= corpsesCopy[c]['died'] + corpsesCopy[c]['TTL']:
-                        # print("deleting " + corpses[corpse]['name'])
-                        del corpses[c]
+        removeCorpses(corpses)
 
         npcRespawns(npcs)
 
---
+++ reaper.py
@@ -0,0 +1,80 @@
+__filename__ = "reaper.py"
+__author__ = "Bob Mottram"
+__credits__ = ["Bob Mottram"]
+__license__ = "AGPL3+"
+__version__ = "1.0.0"
+__maintainer__ = "Bob Mottram"
+__email__ = "bob@freedombone.net"
+__status__ = "Production"
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#                   ...
+#                 ;::::;
+#               ;::::; :;
+#             ;:::::'   :;
+#            ;:::::;     ;.
+#           ,:::::'       ;           OOO\
+#           ::::::;       ;          OOOOO\
+#           ;:::::;       ;         OOOOOOOO
+#          ,;::::::;     ;'         / OOOOOOO
+#        ;:::::::::`. ,,,;.        /  / DOOOOOO
+#      .';:::::::::::::::::;,     /  /     DOOOO
+#     ,::::::;::::::;;;;::::;,   /  /        DOOO
+#    ;`::::::`'::::::;;;::::: ,#/  /          DOOO
+#    :`:::::::`;::::::;;::: ;::#  /            DOOO
+#    ::`:::::::`;:::::::: ;::::# /              DOO
+#    `:`:::::::`;:::::: ;::::::#/               DOO
+#     :::`:::::::`;; ;:::::::::##                OO
+#     ::::`:::::::`;::::::::;:::#                OO
+#     `:::::`::::::::::::;'`:;::#                O
+#      `:::::`::::::::;' /  / `:#
+#       ::::::`:::::;'  /  /   `#
+
+import os
+from functions import log
+from functions import moveNPCs
+from functions import addToScheduler
+from random import randint
+from copy import deepcopy
+
+import time
+
+def removeCorpses(corpses):
+        # Iterate through corpses and remove ones older than their TTL
+        corpsesCopy = deepcopy(corpses)
+        for (c, pl) in corpsesCopy.items():
+                if int(time.time()) >= corpsesCopy[c]['died'] + corpsesCopy[c]['TTL']:
+                        # print("deleting " + corpses[corpse]['name'])
+                        del corpses[c]
+
+def runDeaths(mud,players,corpses,fights):
+        # Handle Player Deaths
+        for (pid, pl) in list(players.items()):
+                if players[pid]['authenticated'] == True:
+                        if players[pid]['hp'] <= 0:
+                                # Create player's corpse in the room
+                                corpses[len(corpses)] = { 'room': players[pid]['room'], 'name': str(players[pid]['name'] + '`s corpse'), 'inv': players[pid]['inv'], 'died': int(time.time()), 'TTL': players[pid]['corpseTTL'], 'owner': 1 }
+                                # Clear player's inventory, it stays on the corpse
+                                # This is bugged, causing errors when picking up things after death
+                                # players[pid]['inv'] = ''
+                                players[pid]['isInCombat'] = 0
+                                players[pid]['lastRoom'] = players[pid]['room']
+                                players[pid]['room'] = '$rid=666$'
+                                fightsCopy = deepcopy(fights)
+                                for (fight, pl) in fightsCopy.items():
+                                        if fightsCopy[fight]['s1id'] == pid or fightsCopy[fight]['s2id'] == pid:
+                                                del fights[fight]
+                                for (pid2, pl) in list(players.items()):
+                                        if players[pid2]['authenticated'] is not None \
+                                           and players[pid2]['room'] == players[pid]['lastRoom'] \
+                                           and players[pid2]['name'] != players[pid]['name']:
+                                                mud.send_message(pid2, '<u><f32>{}<r> <f124>has been killed.'.format(players[pid]['name']) + "\n")
+                                                players[pid]['lastRoom'] = None
+                                                mud.send_message(pid, '<b88><f158>Oh dear! You have died!\n')
+
+                                # Add Player Death event (ID:666) to eventSchedule
+                                addToScheduler(666, pid, eventSchedule, scriptedEventsDB)
+
+                                players[pid]['hp'] = 4
