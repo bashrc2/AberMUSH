68d85c7398ad17d3551004b323bbe1e2c2bb8b3c
Bob Mottram <bob@freedombone.net>
2019-12-02 20:57:31 +0000

    Lower case

--- gcos.py
+++ gcos.py
@@ -18,13 +18,19 @@ def terminalEmulator(command: str,mud,id) -> bool:
     if command=='ls' or command.startswith('ls ') or \
        command=='dir' or command.startswith('dir '):
         mud.send_message(
-            id, "<f220>drwxr-xr-x 105   09:41 README.TXT")
+            id, "<f220>drwxr-xr-x 105   09:41 am/")
         mud.send_message(
-            id, "<f220>drwxr-xr-x 92387 12:17 SPOOL")
+            id, "<f220>drwxr-xr-x 105   09:41 ddd/")
         mud.send_message(
-            id, "<f220>drwxr-xr-x 539   12:17 TELNET/")
+            id, "<f220>drwxr-xr-x 105   09:41 gdd/")
         mud.send_message(
-            id, "<f220>drwxr-xr-x 7252  09:48 USERS.TXT")
+            id, "<f220>drwxr-xr-x 105   09:41 pdd/")
+        mud.send_message(
+            id, "<f220>drwxr-xr-x 105   09:41 sci/")
+        mud.send_message(
+            id, "<f220>drwxr-xr-x 105   09:41 sss/")
+        mud.send_message(
+            id, "<f220>drwxr-xr-x 105   09:41 udd/")
         mud.send_message(
             id, "\n>")
         return True        
@@ -41,74 +47,81 @@ def terminalEmulator(command: str,mud,id) -> bool:
 
     if command.startswith('telnet'):
         mud.send_message(
-            id, "<f220>CONNECTED TO TAPE DRIVE 0")
+            id, "<f220>Connected to tape drive 0")
         mud.send_message(
-            id, "\n<f220>DATANET 1200 OPEN")
+            id, "\n<f220>Datanet 1200 open")
         mud.send_message(
             id, "\n>")
         return True        
     
     if command=='cat' or command=='chown':
         mud.send_message(
-            id, "<f220>LOAD TAPE DRIVE")
+            id, "<f220>Load tape drive")
         mud.send_message(
             id, "\n>")
         return True        
 
-    if command.startswith('mount ') or command=='mount':
+    if command=='mount' or command=='unmount' or command=='umount':
         mud.send_message(
-            id, "<f220>TAPE DRIVE 1 MOUNTED")
+            id, "<f220>Tape drive 1 spinup")
+        mud.send_message(
+            id, "\n>")
+        return True
+
+    if command.startswith('mount ') or command.startswith('unmount '):
+        mud.send_message(
+            id, "<f220>Tape drive 1 mounted")
         mud.send_message(
             id, "\n>")
         return True        
 
     if command.startswith('shred ') or command=='dd' or command.startswith('dd '):
         mud.send_message(
-            id, "<f220>TAPE DRIVE ERROR 7291")
+            id, "<f220>ERROR 7291")
         mud.send_message(
             id, "\n>")
         return True
 
     if command.startswith('chmod'):
         mud.send_message(
-            id, "<f220>CONFIRMED")
+            id, "<f220>Confirmed")
         mud.send_message(
             id, "\n>")
         return True
 
     if command=='pwd' or command.startswith('dirname'):
         mud.send_message(
-            id, "<f220>/HOME/CORMORANT")
+            id, "<f220>/udd/acrc/cormorant")
         mud.send_message(
             id, "\n>")
         return True
 
     if command.startswith('nmap '):
         mud.send_message(
-            id, "<f220>PRESS PLAY")
+            id, "<f220>Press START")
         mud.send_message(
             id, "\n>")
         return True
 
     if command.startswith('shutdown') or command.startswith('reset'):
         mud.send_message(
-            id, "<f220>TAPE DRIVE 0 SPINDOWN")
+            id, "<f220>Tape drive 0 spindown")
         mud.send_message(
-            id, "<f220>TAPE DRIVE 1 SPINDOWN")
+            id, "<f220>Tape drive 1 spindown")
         mud.send_message(
             id, ">")
         return True
 
     if command=='uname' or command.startswith('arch '):
         mud.send_message(
-            id, "<f220>GCOS TSS")
+            id, "<f220>GCOS-3 TSS")
         mud.send_message(
             id, "\n>")
         return True
 
     if command=='who' or command=='whoami' or command.startswith('whois '):
         mud.send_message(
-            id, "<f220>CORMORANT")
+            id, "<f220>cormorant")
         mud.send_message(
             id, "<f220>Data Services Division. Room 5A")
         mud.send_message(
@@ -119,7 +132,7 @@ def terminalEmulator(command: str,mud,id) -> bool:
 
     if command.startswith('useradd') or command.startswith('adduser'):
         mud.send_message(
-            id, "<f220>REWIND TAPE")
+            id, "<f220>Rewind tape drive 1")
         mud.send_message(
             id, "\n>")
         return True        
@@ -127,7 +140,9 @@ def terminalEmulator(command: str,mud,id) -> bool:
     invalidNames=("sh","bash","chcon","chgrp","chown","chmod","cp","cd","dd","df","dir","dircolors","install","ln","ls","mkdir","mkfifo","mknod","mktemp","mv","realpath","rm","rmdir","shred","sync","touch","truncate","vdir","b2sum","base32","base64","cat","cksum","comm","csplit","cut","expand","fmt","fold","head","join","md5sum","nl","numfmt","od","paste","ptx","pr","sha1sum","sha224sum","sha256sum","sha384sum","sha512sum","shuf","sort","split","sum","tac","tail","tr","tsort","unexpand","uniq","wc","arch","basename","chroot","date","dirname","du","echo","env","expr","factor","false","groups","hostid","id","link","logname","nice","nohup","nproc","pathchk","pinky","printenv","printf","pwd","readlink","runcon","seq","sleep","stat","stdbuf","stty","tee","test","timeout","true","tty","uname","unlink","uptime","users","useradd","adduser","who","whoami","yes")
     if command in invalidNames:
         mud.send_message(
-            id, "\n<f220>GCOS-III TSS Aberystwyth Computing Research Centre (Channel d.h000)")
+            id, "\n<f220>GCOS-3 Time Sharing Environment")
+        mud.send_message(
+            id, "<f220>Aberystwyth Computing Research Centre (Channel d.h000)")
         mud.send_message(
             id, "<f220>Load = 7.0 out of 90.0 units: users = 7, 14/07/1989  1531.6 gmt Sun")
         mud.send_message(
