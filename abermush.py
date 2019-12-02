0d29f65ed2d26193808024c956467b85e7c74d96
Bob Mottram <bob@freedombone.net>
2019-12-02 13:08:23 +0000

    Only activated if other conditions don't apply

--- abermush.py
+++ abermush.py
@@ -621,24 +621,25 @@ while True:
                         taken = True
                         break
 
-            # easteregg when trying to log in with a unix command
-            invalidNames=("chcon","chgrp","chown","chmod","cp","dd","df","dir","dircolors","install","ln","ls","mkdir","mkfifo","mknod","mktemp","mv","realpath","rm","rmdir","shred","sync","touch","truncate","vdir","b2sum","base32","base64","cat","cksum","comm","csplit","cut","expand","fmt","fold","head","join","md5sum","nl","numfmt","od","paste","ptx","pr","sha1sum","sha224sum","sha256sum","sha384sum","sha512sum","shuf","sort","split","sum","tac","tail","tr","tsort","unexpand","uniq","wc","arch","basename","chroot","date","dirname","du","echo","env","expr","factor","false","groups","hostid","id","link","logname","nice","nohup","nproc","pathchk","pinky","printenv","printf","pwd","readlink","runcon","seq","sleep","stat","stdbuf","stty","tee","test","timeout","true","tty","uname","unlink","uptime","users","who","whoami","yes")
-            if command.strip().lower() in invalidNames:
-                mud.send_message(
-                    id, "\n<f220>Multics MR12.5 Aberystwyth Computing Research Centre (Channel d.h000)")
-                mud.send_message(
-                    id, "\n<f220>Load = 7.0 out of 90.0 units: users = 7, 14/07/1989  1531.6 gmt Sun")
-                mud.send_message(
-                    id, "\n<f220>You are protected from preemption.")
-                mud.send_message(
-                    id, "\n<f220>Logged in from ASCII terminal \"cormorant\"")
-                mud.send_message(
-                    id, "\n\n<f220>New messages in message_of_the_day:")
-                mud.send_message(
-                    id, "\n\n<f220>Welcome to the Multics System.")
-                mud.send_message(id, "\n>\n\n")
-                taken = True
-                break
+            if not taken:
+                # easteregg when trying to log in with a unix command
+                invalidNames=("chcon","chgrp","chown","chmod","cp","dd","df","dir","dircolors","install","ln","ls","mkdir","mkfifo","mknod","mktemp","mv","realpath","rm","rmdir","shred","sync","touch","truncate","vdir","b2sum","base32","base64","cat","cksum","comm","csplit","cut","expand","fmt","fold","head","join","md5sum","nl","numfmt","od","paste","ptx","pr","sha1sum","sha224sum","sha256sum","sha384sum","sha512sum","shuf","sort","split","sum","tac","tail","tr","tsort","unexpand","uniq","wc","arch","basename","chroot","date","dirname","du","echo","env","expr","factor","false","groups","hostid","id","link","logname","nice","nohup","nproc","pathchk","pinky","printenv","printf","pwd","readlink","runcon","seq","sleep","stat","stdbuf","stty","tee","test","timeout","true","tty","uname","unlink","uptime","users","who","whoami","yes")
+                if command.strip().lower() in invalidNames:
+                    mud.send_message(
+                        id, "\n<f220>Multics MR12.5 Aberystwyth Computing Research Centre (Channel d.h000)")
+                    mud.send_message(
+                        id, "\n<f220>Load = 7.0 out of 90.0 units: users = 7, 14/07/1989  1531.6 gmt Sun")
+                    mud.send_message(
+                        id, "\n<f220>You are protected from preemption.")
+                    mud.send_message(
+                        id, "\n<f220>Logged in from ASCII terminal \"cormorant\"")
+                    mud.send_message(
+                        id, "\n\n<f220>New messages in message_of_the_day:")
+                    mud.send_message(
+                        id, "\n\n<f220>Welcome to the Multics System.")
+                    mud.send_message(id, "\n>\n\n")
+                    taken = True
+                    break
                     
             if not taken:
                 players[id]['exAttribute1'] = command.strip()
