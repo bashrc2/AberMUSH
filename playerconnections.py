ba53a58fb86514b6269b52484f9afc59b8171d5e
Bob Mottram <bob@freedombone.net>
2019-12-07 23:04:52 +0000

    Tidying

--- playerconnections.py
+++ playerconnections.py
@@ -229,3 +229,127 @@ def playerInGame(id,username: str,players: {}) -> bool:
            pl != id:
             return True
     return False
+
+def initialSetupAfterLogin(mud,id,players: {},dbResponse: []):
+    """Sets up a player after login
+    """
+    players[id]['authenticated'] = True
+    players[id]['prefix'] = "None"
+    players[id]['room'] = dbResponse[1]
+    players[id]['lvl'] = dbResponse[2]
+    players[id]['exp'] = dbResponse[3]
+    players[id]['str'] = dbResponse[4]
+    players[id]['siz'] = dbResponse[5]
+    players[id]['wei'] = dbResponse[6]
+    players[id]['per'] = dbResponse[7]
+    players[id]['endu'] = dbResponse[8]
+    players[id]['cha'] = dbResponse[9]
+    players[id]['int'] = dbResponse[10]
+    players[id]['agi'] = dbResponse[11]
+    players[id]['luc'] = dbResponse[12]
+    players[id]['cool'] = dbResponse[13]
+    players[id]['cred'] = dbResponse[14]
+    players[id]['inv'] = dbResponse[15]  # .split(',')
+    players[id]['speakLanguage'] = dbResponse[16]
+    players[id]['language'] = dbResponse[17]
+    players[id]['convstate'] = dbResponse[18]  # .split(',')
+    # Example: item_list = [e for e in item_list if e not in ('item', 5)]
+    #players[id]['inv'] = [e for e in players[id]['inv'] if e not in ('', ' ')]
+    players[id]['clo_head'] = dbResponse[20]
+    players[id]['clo_neck'] = dbResponse[21]
+    players[id]['clo_larm'] = dbResponse[22]
+    players[id]['clo_rarm'] = dbResponse[23]
+    players[id]['clo_lhand'] = dbResponse[24]
+    players[id]['clo_rhand'] = dbResponse[25]
+    players[id]['clo_lwrist'] = dbResponse[26]
+    players[id]['clo_rwrist'] = dbResponse[27]
+    players[id]['clo_chest'] = dbResponse[28]
+    players[id]['clo_lleg'] = dbResponse[29]
+    players[id]['clo_rleg'] = dbResponse[30]
+    players[id]['clo_feet'] = dbResponse[31]
+    players[id]['imp_head'] = dbResponse[32]
+    players[id]['imp_larm'] = dbResponse[33]
+    players[id]['imp_rarm'] = dbResponse[34]
+    players[id]['imp_lhand'] = dbResponse[35]
+    players[id]['imp_rhand'] = dbResponse[36]
+    players[id]['imp_chest'] = dbResponse[37]
+    players[id]['imp_lleg'] = dbResponse[38]
+    players[id]['imp_rleg'] = dbResponse[39]
+    players[id]['imp_feet'] = dbResponse[40]
+    players[id]['hpMax'] = dbResponse[41]
+    players[id]['hp'] = dbResponse[42]
+    players[id]['charge'] = dbResponse[43]
+    players[id]['inDescription'] = dbResponse[44]
+    players[id]['outDescription'] = dbResponse[45]
+    players[id]['lookDescription'] = dbResponse[46]
+    players[id]['isInCombat'] = 0
+    players[id]['lastCombatAction'] = int(time.time())
+    players[id]['isAttackable'] = 1
+    players[id]['corpseTTL'] = 60
+    players[id]['idleStart'] = int(time.time())
+    players[id]['channels'] = dbResponse[47]
+    players[id]['permissionLevel'] = dbResponse[48]
+    players[id]['exAttribute0'] = dbResponse[49]
+    players[id]['exAttribute1'] = dbResponse[50]
+    players[id]['exAttribute2'] = dbResponse[51]
+    players[id]['canGo'] = dbResponse[52]
+    players[id]['canLook'] = dbResponse[53]
+    players[id]['canSay'] = dbResponse[54]
+    players[id]['canAttack'] = dbResponse[55]
+    players[id]['canDirectMessage'] = dbResponse[56]
+    players[id]['ref'] = dbResponse[57]
+    players[id]['bodyType'] = dbResponse[58]
+    players[id]['race'] = dbResponse[59]
+    players[id]['characterClass'] = dbResponse[60]
+    players[id]['proficiencies'] = dbResponse[61]
+    players[id]['fightingStyle'] = dbResponse[62]
+    players[id]['restRequired'] = dbResponse[63]
+    players[id]['enemy'] = dbResponse[64]
+    players[id]['tempCharm'] = dbResponse[65]
+    players[id]['tempCharmTarget'] = dbResponse[66]
+    players[id]['tempCharmDuration'] = dbResponse[67]
+    players[id]['tempCharmStart'] = dbResponse[68]
+    players[id]['guild'] = dbResponse[69]
+    players[id]['guildRole'] = dbResponse[70]
+    players[id]['archetype'] = dbResponse[71]
+    players[id]['preparedSpells'] = dbResponse[72]
+    players[id]['spellSlots'] = dbResponse[73]
+    players[id]['tempHitPoints'] = dbResponse[74]
+    players[id]['tempHitPointsStart'] = dbResponse[75]
+    players[id]['tempHitPointsDuration'] = dbResponse[76]
+    players[id]['prepareSpell'] = dbResponse[77]
+    players[id]['prepareSpellProgress'] = dbResponse[78]
+    players[id]['prepareSpellTime'] = dbResponse[79]
+    players[id]['frozenDescription'] = dbResponse[80]
+    players[id]['frozenStart'] = dbResponse[81]
+    players[id]['frozenDuration'] = dbResponse[82]
+    players[id]['affinity'] = dbResponse[83]
+    players[id]['familiar'] = dbResponse[84]
+    if players[id].get('visibleWhenWearing'):
+        players[id]['visibleWhenWearing'] = dbResponse[85]
+
+    log("Client ID: " +
+        str(id) +
+        " has successfully authenticated user " +
+        players[id]['name'], "info")
+
+    # print(players[id])
+    # go through all the players in the game
+    for (pid, pl) in list(players.items()):
+        # send each player a message to tell them about the new
+        # player
+        if players[pid]['authenticated'] is not None \
+           and players[pid]['room'] == players[id]['room'] \
+           and players[pid]['name'] != players[id]['name']:
+            mud.send_message(
+                pid, '{} has materialised out of thin air nearby.'.format(
+                    players[id]['name']) + "\n\n")
+
+    # send the new player a welcome message
+    mud.send_message(id, \
+                     '\n<f220>Welcome to AberMUSH!, {}. '.format(players[id]['name']))
+    mud.send_message(id, \
+                     '\n<f255>Hello there traveller! You have connected to an AberMUSH server. You can move around the rooms along with other players (if you are lucky to meet any), attack each other (including NPCs), pick up and drop items and chat. Make sure to visit the repo for further info. Thanks for your interest in AberMUSH.')
+    mud.send_message(id, \
+        "\n<f255>Type '<r><f220>help<r><f255>' for a list of all currently implemented commands/functions. Have fun!\n\n")
+    
