8e6eae2889766740e7d5f850571e390a1f538a15
Bob Mottram <bob@freedombone.net>
2020-02-07 13:08:29 +0000

    Text color

--- playerconnections.py
+++ playerconnections.py
@@ -152,7 +152,7 @@ def runNewPlayerConnections(mud, id, players, playersDB, fights, Config):
         if not os.path.isfile(".disableRegistrations"):
             mud.send_message_wrap(
                 id, '<f220>', \
-                '<f15>You can create a new Character, or use the following guest account:')
+                'You can create a new Character, or use the following guest account:')
             mud.send_message(
                 id, '<f15>Username: <r><f220>Guest<r><f15> Password: <r><f220>Password')
             mud.send_message(
