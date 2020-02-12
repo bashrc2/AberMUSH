94afb25fff0ee7025912f16c3d4a79ad0cb79655
Bob Mottram <bob@freedombone.net>
2020-02-12 20:09:35 +0000

    Debug

--- commands.py
+++ commands.py
@@ -4175,9 +4175,10 @@ def chess(
         if len(items[boardItemID]['gameState']['history'])<2:
             params='reset'
         else:
-            mud.send_message(id, '\nUndoing last chess move '+str(len(items[boardItemID]['gameState']['history']))+'.\n')
+            mud.send_message(id, '\nUndoing last chess move.\n')
+            mud.send_message(id, 'History1 '+str(items[boardItemID]['gameState']['history'][-1])+'.\n')
             items[boardItemID]['gameState']['history'].pop()
-            mud.send_message(id, 'History length '+str(len(items[boardItemID]['gameState']['history']))+'.\n')
+            mud.send_message(id, 'History2 '+str(items[boardItemID]['gameState']['history'][-1])+'.\n')
             gameState=items[boardItemID]['gameState']['history'][-1]            
             items[boardItemID]['gameState']['state']=gameState
             if items[boardItemID]['gameState']['turn']=='white':
