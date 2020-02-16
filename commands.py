ba79d5c806158abfe287c30a349036a6d859b587
Bob Mottram <bob@freedombone.net>
2020-02-16 18:11:59 +0000

    Wrap climbing message

--- commands.py
+++ commands.py
@@ -3977,7 +3977,9 @@ def climb(
             # update the player's current room to the one the exit leads to
             players[id]['room'] = targetRoom
             # climbing message
-            mud.send_message(id, randomDescription(itemsDB[itemId]['climbThrough'])+"\n\n")
+            mud.send_message_wrap(id,'<f230>', \
+                                  randomDescription(itemsDB[itemId]['climbThrough'])+"\n\n")
+            time.sleep(3)
             # trigger new room eventOnEnter for the player
             if rooms[players[id]['room']]['eventOnEnter'] is not "":
                 addToScheduler(int(rooms[players[id]['room']]['eventOnEnter']),
