d08d10d0d28ed8da1e47193f48efe5e8edd4cc82
Bob Mottram <bob@freedombone.net>
2020-03-19 13:10:01 +0000

    Change polarity

--- commands.py
+++ commands.py
@@ -2029,7 +2029,7 @@ def getRoomExits(rooms: {},players: {},id) -> {}:
         directionsAdded=[]
         tideState=runTide()
         for direction,roomID in rm['tideOutExits'].items():
-            if tideState < 0:
+            if tideState > 0:
                 exits[direction]=roomID
                 # keep track of directions added
                 directionsAdded.append(direction)
