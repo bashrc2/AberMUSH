e376aaa2570dea3f95d8ed6eaf4fe2d81a133f61
Bob Mottram <bob@freedombone.net>
2020-04-09 10:31:10 +0100

    flake8 format

--- chess.py
+++ chess.py
@@ -6,10 +6,7 @@ __maintainer__ = "Bob Mottram"
 __email__ = "bob@freedombone.net"
 __status__ = "Production"
 
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-import re, sys
+import re
 
 initial = [
     'rnbqkbnr',
@@ -23,74 +20,80 @@ initial = [
 ]
 
 uni_pieces = {
-    'R':'♜', 'N':'♞', 'B':'♝', 'Q':'♛', 'K':'♚', 'P':'♟', \
-    'r':'♖', 'n':'♘', 'b':'♗', 'q':'♕', 'k':'♔', 'p':'♙', '.':'·'
+    'R': '♜', 'N': '♞', 'B': '♝', 'Q': '♛', 'K': '♚', 'P': '♟',
+    'r': '♖', 'n': '♘', 'b': '♗', 'q': '♕', 'k': '♔', 'p': '♙', '.': '·'
 }
 
-def showChessBoard(gameState: [],id,mud,turn: str) -> None:
+
+def showChessBoard(gameState: [], id, mud, turn: str) -> None:
     """Shows the chess board
     """
     mud.send_message(id, '\n')
-    boardStr=''
-    i=0
-    if turn=='white':
+    boardStr = ''
+    i = 0
+    if turn == 'white':
         for row in gameState:
-            boardRowStr=' '+str(8-i)+' '
+            boardRowStr = ' ' + str(8 - i) + ' '
             for p in row:
-                boardRowStr+=' '+uni_pieces[p]
-            boardStr+=boardRowStr+'\n'
-            i+=1
-        boardStr+='\n    a b c d e f g h \n\n'
+                boardRowStr += ' ' + uni_pieces[p]
+            boardStr += boardRowStr + '\n'
+            i += 1
+        boardStr += '\n    a b c d e f g h \n\n'
     else:
         for row in gameState:
-            boardRowStr=''
+            boardRowStr = ''
             for p in row:
-                boardRowStr=' '+uni_pieces[p]+boardRowStr
-            boardStr=' '+str(8-i)+' '+boardRowStr+'\n'+boardStr
-            i+=1
-        boardStr+='\n    h g f e d c b a \n\n'
-    mud.send_game_board(id,boardStr)
+                boardRowStr = ' ' + uni_pieces[p] + boardRowStr
+            boardStr = ' ' + str(8 - i) + ' ' + boardRowStr + '\n' + boardStr
+            i += 1
+        boardStr += '\n    h g f e d c b a \n\n'
+    mud.send_game_board(id, boardStr)
+
 
 def initialChessBoard() -> []:
     """Returns the initial state of a chess game
     """
     return initial.copy()
 
-def chessPieceAt(gameState: [],coord: str) -> str:
-    if len(coord)!=2:
-        return '.'
-    if ord(coord[0])<ord('a') or ord(coord[0])>ord('h'):
-        return '.'
-    if ord(coord[1])<ord('1') or ord(coord[1])>ord('8'):
-        return '.'
-    return gameState[ord('8')-ord(coord[1])][ord(coord[0])-ord('a')]
 
-def chessPieceSet(gameState: [],coord: str,piece: str) -> None:
-    if len(coord)!=2:
-        return
-    if ord(coord[0])<ord('a') or ord(coord[0])>ord('h'):
-        return
-    if ord(coord[1])<ord('1') or ord(coord[1])>ord('8'):
-        return
-    row=gameState[ord('8')-ord(coord[1])]
-    col=ord(coord[0])-ord('a')
-    gameState[ord('8')-ord(coord[1])]=row[:col]+piece+row[(col+1):]
+def chessPieceAt(gameState: [], coord: str) -> str:
+    if len(coord) != 2:
+        return '.'
+    if ord(coord[0]) < ord('a') or ord(coord[0]) > ord('h'):
+        return '.'
+    if ord(coord[1]) < ord('1') or ord(coord[1]) > ord('8'):
+        return '.'
+    return gameState[ord('8') - ord(coord[1])][ord(coord[0]) - ord('a')]
 
-def moveChessPiece(moveStr: str,gameState: [],turn: str,id,mud) -> bool:
+
+def chessPieceSet(gameState: [], coord: str, piece: str) -> None:
+    if len(coord) != 2:
+        return
+    if ord(coord[0]) < ord('a') or ord(coord[0]) > ord('h'):
+        return
+    if ord(coord[1]) < ord('1') or ord(coord[1]) > ord('8'):
+        return
+    row = gameState[ord('8') - ord(coord[1])]
+    col = ord(coord[0]) - ord('a')
+    gameState[ord('8') - ord(coord[1])] = \
+        row[:col] + piece + row[(col + 1):]
+
+
+def moveChessPiece(moveStr: str, gameState: [], turn: str, id, mud) -> bool:
     match = re.match('([a-h][1-8])'*2, moveStr.lower())
     if not match:
         return False
-    moveFrom=match.group(1)
-    moveTo=match.group(2)
-    fromPiece=chessPieceAt(gameState,moveFrom)
-    if fromPiece=='.':
+    moveFrom = match.group(1)
+    moveTo = match.group(2)
+    fromPiece = chessPieceAt(gameState, moveFrom)
+    if fromPiece == '.':
         return False
-    if turn=='white':
-        if fromPiece.upper()!=fromPiece:
+    if turn == 'white':
+        if fromPiece.upper() != fromPiece:
             return False
     else:
-        if fromPiece.lower()!=fromPiece:
+        if fromPiece.lower() != fromPiece:
             return False
-    chessPieceSet(gameState,moveFrom,'.')
-    chessPieceSet(gameState,moveTo,fromPiece)
+    chessPieceSet(gameState, moveFrom, '.')
+    chessPieceSet(gameState, moveTo, fromPiece)
     return True
