979d99fd30cb3fb9c6d0016d33ef4f7ad103857a
Bob Mottram <bob@freedombone.net>
2020-09-05 22:22:36 +0100

    Websocket classes

---
+++ mudserverws.py
@@ -0,0 +1,48 @@
+__filename__ = "mudserverws.py"
+__author__ = "Bob Mottram"
+__credits__ = ["Dave P. https://github.com/dpallot"]
+__license__ = "AGPL3+"
+__version__ = "1.0.0"
+__maintainer__ = "Bob Mottram"
+__email__ = "bob@freedombone.net"
+__status__ = "Production"
+
+'''
+   import signal
+   import sys
+   from WebSocketServer import WebSocket, WebSocketServer
+   import mudserverws.py
+
+   server = SimpleWebSocketServer('localhost', port, MUDServerWS)
+
+   def close_sig_handler(signal, frame):
+      server.close()
+      sys.exit()
+
+   signal.signal(signal.SIGINT, close_sig_handler)
+
+   server.serveforever()
+'''
+
+from WebSocketServer import WebSocket
+
+clients = []
+
+
+class MUDServerWS(WebSocket):
+    def handleMessage(self):
+        for client in clients:
+            if client != self:
+                client.sendMessage(self.address[0] + u' - ' + self.data)
+
+    def handleConnected(self):
+        print(self.address, 'connected')
+        for client in clients:
+            client.sendMessage(self.address[0] + u' - connected')
+        clients.append(self)
+
+    def handleClose(self):
+        clients.remove(self)
+        print(self.address, 'closed')
+        for client in clients:
+            client.sendMessage(self.address[0] + u' - disconnected')
