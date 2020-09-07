337c148f374077bc1f592645f9b00cfdb44a70e4
Bob Mottram <bob@freedombone.net>
2020-09-07 11:46:08 +0100

    Scroll up

--- mudserver.py
+++ mudserver.py
@@ -322,18 +322,15 @@ class MudServer(object):
             # all of the data is sent in one go
             cl = self._clients[to]
             linectr = len(messageLines)
-            if cl.client_type == self._CLIENT_TELNET:
-                for lineStr in messageLines:
-                    if linectr <= 30:
-                        msgStr = lineStr + '\n'
-                        cl.socket.sendall(bytearray(msgStr, 'utf-8'))
-                        time.sleep(0.03)
-                    linectr -= 1
-            elif cl.client_type == self._CLIENT_WEBSOCKET:
-                for lineStr in messageLines:
+            for lineStr in messageLines:
+                if linectr <= 30:
                     msgStr = lineStr + '\n'
-                    cl.socket.sendMessage(msgStr)
-                # cl.socket.sendMessage(message)
+                    if cl.client_type == self._CLIENT_TELNET:
+                        cl.socket.sendall(bytearray(msgStr, 'utf-8'))
+                    elif cl.client_type == self._CLIENT_WEBSOCKET:
+                        cl.socket.sendMessage(msgStr)
+                    time.sleep(0.03)
+                linectr -= 1
             if cl.client_type == self._CLIENT_TELNET:
                 cl.socket.sendall(bytearray(cmsg('<b0>'), 'utf-8'))
             elif cl.client_type == self._CLIENT_WEBSOCKET:
