97c97342055f1c1312aeb412ca0987264f6564ae
Bob Mottram <bob@freedombone.net>
2022-01-07 11:14:14 +0000

    Snake case

--- markets.py
+++ markets.py
@@ -10,7 +10,7 @@ __module_group__ = ""
 from environment import get_room_culture
 
 
-exchangeRate = {
+EXCHANGE_RATE = {
     "cp": {
         "cp": 1,
         "sp": 10,
@@ -52,130 +52,130 @@ exchangeRate = {
 def money_purchase(id, players: {}, cost: str) -> bool:
     """Does the player have enough money to buy something at the given cost?
     """
-    costDenom = None
-    costValue = 0
-    for denom, ex in exchangeRate.items():
+    cost_denom = None
+    cost_value = 0
+    for denom, _ in EXCHANGE_RATE.items():
         if not cost.endswith(denom):
             continue
-        costStr = cost.replace(denom, '')
-        if not costStr.isdigit():
+        cost_str = cost.replace(denom, '')
+        if not cost_str.isdigit():
             return False
-        costValue = int(costStr)
-        costDenom = denom
+        cost_value = int(cost_str)
+        cost_denom = denom
 
-    if not costDenom:
+    if not cost_denom:
         return False
 
-    for denom, qty in exchangeRate[costDenom].items():
-        minQty = int((1.0 / qty) * costValue)
-        if players[id][denom] >= minQty:
-            players[id][denom] -= minQty
+    for denom, qty in EXCHANGE_RATE[cost_denom].items():
+        min_qty = int((1.0 / qty) * cost_value)
+        if players[id][denom] >= min_qty:
+            players[id][denom] -= min_qty
             return True
     return False
 
 
-def get_market_type(roomName: str, markets: {}) -> str:
+def get_market_type(room_name: str, markets: {}) -> str:
     """Returns the market type for the room name
     """
-    for marketType, item in markets.items():
-        if '|' not in marketType:
-            if marketType.lower() in roomName:
-                return marketType
+    for market_type, _ in markets.items():
+        if '|' not in market_type:
+            if market_type.lower() in room_name:
+                return market_type
         else:
-            marketTypeList = marketType.lower().split('|')
-            for marketType2 in marketTypeList:
-                if marketType2 in roomName:
-                    return marketType
+            market_type_list = market_type.lower().split('|')
+            for market_type2 in market_type_list:
+                if market_type2 in room_name:
+                    return market_type
     return None
 
 
-def _marketSellsItemTypes(marketType: str, markets: {}) -> []:
+def _market_sells_item_types(market_type: str, markets: {}) -> []:
     """Returns a list of item types which a market sells
     """
-    marketSells = []
-    if markets[marketType].get('trades'):
-        marketSells = markets[marketType]['trades']
-    if markets[marketType].get('sells'):
-        if not marketSells:
-            marketSells = markets[marketType]['sells']
+    market_sells = []
+    if markets[market_type].get('trades'):
+        market_sells = markets[market_type]['trades']
+    if markets[market_type].get('sells'):
+        if not market_sells:
+            market_sells = markets[market_type]['sells']
         else:
-            for itemType in markets[marketType]['sells']:
-                if itemType not in marketSells:
-                    marketSells.append(itemType)
-    return marketSells
+            for item_type in markets[market_type]['sells']:
+                if item_type not in market_sells:
+                    market_sells.append(item_type)
+    return market_sells
 
 
-def market_buys_item_types(marketType: str, markets: {}) -> []:
+def market_buys_item_types(market_type: str, markets: {}) -> []:
     """Returns a list of item types which a market buys
     """
-    marketBuys = []
-    if markets[marketType].get('buys'):
-        marketBuys = markets[marketType]['buys']
-    if markets[marketType].get('trades'):
-        if not marketBuys:
-            marketBuys = markets[marketType]['trades']
+    market_buys = []
+    if markets[market_type].get('buys'):
+        market_buys = markets[market_type]['buys']
+    if markets[market_type].get('trades'):
+        if not market_buys:
+            market_buys = markets[market_type]['trades']
         else:
-            for itemType in markets[marketType]['trades']:
-                if itemType not in marketBuys:
-                    marketBuys.append(itemType)
-    return marketBuys
+            for item_type in markets[market_type]['trades']:
+                if item_type not in market_buys:
+                    market_buys.append(item_type)
+    return market_buys
 
 
 def assign_markets(markets: {}, rooms: {}, items_db: {},
                    cultures_db: {}) -> int:
     """Assigns market types to rooms
     """
-    noOfMarkets = 0
+    no_of_markets = 0
     for room_id, rm in rooms.items():
         # whether or not there is a market here depends on the room name
         rooms[room_id]['marketInventory'] = None
-        roomName = rm['name'].lower()
-        marketType = get_market_type(roomName, markets)
-        if not marketType:
+        room_name = rm['name'].lower()
+        market_type = get_market_type(room_name, markets)
+        if not market_type:
             continue
 
         # get the types of items sold
         rooms[room_id]['marketInventory'] = {}
-        marketSells = _marketSellsItemTypes(marketType, markets)
-        if not marketSells:
+        market_sells = _market_sells_item_types(market_type, markets)
+        if not market_sells:
             continue
 
         # the market can have a culture, which then
         # determines the types of items within it
-        roomCulture = get_room_culture(cultures_db, rooms, room_id)
+        room_culture = get_room_culture(cultures_db, rooms, room_id)
 
-        inventoryCtr = 0
-        itemNamesList = []
-        for itemID, item in items_db.items():
-            for itemType in marketSells:
-                itemName = item['name'].lower()
-                if itemType not in itemName:
+        inventory_ctr = 0
+        item_names_list = []
+        for item_id, item in items_db.items():
+            for item_type in market_sells:
+                item_name = item['name'].lower()
+                if item_type not in item_name:
                     continue
-                if itemName in itemNamesList:
+                if item_name in item_names_list:
                     continue
                 if item['weight'] <= 0:
                     continue
-                if roomCulture:
+                if room_culture:
                     if item.get('culture'):
-                        if item['culture'] != roomCulture:
+                        if item['culture'] != room_culture:
                             continue
-                itemNamesList.append(itemName)
-                itemCost = item['cost']
+                item_names_list.append(item_name)
+                item_cost = item['cost']
                 # TODO cost and stock level could vary with
                 # region, coords, season, etc
                 # which could then lead to merchanting
-                rooms[room_id]['marketInventory'][itemID] = {
+                rooms[room_id]['marketInventory'][item_id] = {
                     "stock": 1,
-                    "cost": itemCost
+                    "cost": item_cost
                 }
-                inventoryCtr += 1
+                inventory_ctr += 1
                 break
-        if inventoryCtr > 0:
-            noOfMarkets += 1
-    return noOfMarkets
+        if inventory_ctr > 0:
+            no_of_markets += 1
+    return no_of_markets
 
 
-def buy_item(players: {}, id, itemID, items_db: {}, cost: str) -> bool:
+def buy_item(players: {}, id, item_id, items_db: {}, cost: str) -> bool:
     """Returns true if the given item was bought
     """
     if cost == '0':
